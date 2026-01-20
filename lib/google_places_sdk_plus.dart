6877dbc5a33da727a52d8dca5d07adc71e96787a
Kazymir Rabier <kazymir.rabier@gmail.com>
2026-01-20 14:53:14 +0800

    refactor: update import path for places.g.dart and remove a blank line.

--- lib/google_places_sdk_plus.dart
+++ lib/google_places_sdk_plus.dart
@@ -1,7 +1,6 @@
-import 'package:google_places_sdk_plus/src/places.g.dart';
+import 'src/places.g.dart';
 
 export 'src/places.g.dart';
 export 'src/extensions.dart';
 
 class GooglePlaces extends PlacesHostApi {}
-
